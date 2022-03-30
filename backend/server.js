bd80ee231163b4c6334eb2f5f6beeafa7f547d9b
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-03-30 19:42:38 +0530

    Connected DataBase

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-
+const connectDB = require("./config/db");
+const colors = require("colors");
 const app = express();
 dotenv.config();
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("Api is Running");
 });
@@ -22,5 +23,5 @@ app.get("/api/chat/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server Started on PORT ${PORT}`);
+  console.log(`Server Started on PORT ${PORT}`.yellow.bold);
 });
