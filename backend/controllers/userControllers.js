42d01f56352ac239085ca3f73599d5cee2ffebcb
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-03-31 21:51:05 +0530

    Completed Search User

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -56,4 +56,19 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+//  api/user?search=chirag
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } }); // find except the current user thats why we used $ne
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOMClient from "react-dom/client";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-const container = document.getElementById("root");
-const root = ReactDOMClient.createRoot(container);
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
